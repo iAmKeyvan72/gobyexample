ac2616b32203e70827d17a7f44a5e850a1b0b66d
thek1 <5063636+iAmKeyvan72@users.noreply.github.com>
2023-05-06 14:59:00 +0200

    done

--- main.go
+++ main.go
@@ -2,36 +2,31 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type rect struct {
+	width, height int
 }
 
-func newPerson(name string) *person {
+func (r *rect) multiply(m int) {
+	r.width *= m
+	r.height *= 2 * m
+}
 
-	p := person{name: name}
-	p.age = 42
-	return &p
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
 }
 
 func main() {
+	r := rect{width: 10, height: 5}
 
-	fmt.Println(person{"Bob", 20})
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim:", r.perim())
 
-	fmt.Println(person{name: "Alice", age: 30})
-
-	fmt.Println(person{name: "Fred"})
-
-	fmt.Println(&person{name: "Ann", age: 40})
-
-	fmt.Println(newPerson("Jon"))
-
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	sp := &s
-	fmt.Println(sp.age)
-
-	sp.age = 51
-	fmt.Println(sp.age)
+	rp := &r
+	rp.multiply(2)
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim:", rp.perim())
 }
