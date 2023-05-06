c7c128c9e64279942c1d6e0989004fd47825763a
thek1 <5063636+iAmKeyvan72@users.noreply.github.com>
2023-05-06 07:49:32 +0200

    done

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
-import "fmt"
-
-func zeroVal(n int) {
-	n = 0
-}
-
-func zeroPtr(n *int) {
-	*n = 0
-}
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	i := 10
 
-	fmt.Println(i)
+	const s = "สวัสดี"
 
-	zeroVal(i)
-	fmt.Println(i)
+	fmt.Println("Len:", len(s))
 
-	zeroPtr(&i)
-	fmt.Println(i)
-	fmt.Println(&i)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
 
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune) {
+
+	if r == 't' {
+		fmt.Println("found tee")
+	} else if r == 'ส' {
+		fmt.Println("found so sua")
+	}
 }
