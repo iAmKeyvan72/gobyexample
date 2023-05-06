03f07cd2b8dc463181991aedfec4d0cf7be3f548
thek1 <5063636+iAmKeyvan72@users.noreply.github.com>
2023-05-06 07:09:07 +0200

    done

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
-func sum(nums ...int) {
-	fmt.Print(nums)
-	total := 0
-
-	for _, num := range nums {
-		total += num
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
 	}
-
-	fmt.Println(total)
 }
 
 func main() {
-	sum(1, 2)
+	nextInt := intSeq()
 
-	sum(49, 21, 324, 624)
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
 
-	nums := []int{23, 63, 7567, 6586, 345}
-	sum(nums...)
+	newInts := intSeq()
+	fmt.Println(newInts())
 }
